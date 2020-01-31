1ef1f6fdd34ac714ed25fe5b49271d37b4daf207
IvanBrasilico <brasilico.ivan@gmail.com>
2020-01-31 14:00:06 -0300

    Caminho virasana no path

--- wsgi_production.py
+++ wsgi_production.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from werkzeug.wsgi import DispatcherMiddleware
+
+sys.path.insert(0, '../ajna_docs/commons')
+sys.path.insert(0, '../virasana')
 
 from bhadrasana.main import app
 
