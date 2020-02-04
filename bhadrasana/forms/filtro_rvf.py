6ce869b5f510b722599634491227ac26fa2be3d0
IvanBrasilico <Iv@n1234>
2020-02-04 20:56:10 -0300

    Pesquisa de RVF

--- bhadrasana/forms/filtro_rvf.py
+++ bhadrasana/forms/filtro_rvf.py
@@ -5,7 +5,7 @@ from wtforms import StringField, IntegerField, DateTimeField
 
 class FiltroRVFForm(FlaskForm):
     id = IntegerField('ID')
-    ce = StringField(u'CE Mercante',
+    numeroCEmercante = StringField(u'CE Mercante',
                                    default='')
     datainicio = DateTimeField(u'Data inicial da pesquisa')
     datafim = DateTimeField(u'Data final da pesquisa')
