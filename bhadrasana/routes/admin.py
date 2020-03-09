bfd22f35008d5cf23214c647d2ad28ed2aac76ef
IvanBrasilico <brasilico.ivan@gmail.com>
2020-03-09 15:44:36 -0300

    Pesquisa geral não filtrar Setor

--- bhadrasana/routes/admin.py
+++ bhadrasana/routes/admin.py
@@ -5,7 +5,14 @@ from bhadrasana.models import Setor, Usuario
 
 class UserModel(ModelView):
     column_display_pk = True
-    form_columns = ('cpf', 'nome', 'setor')
+    column_list = ('cpf', 'nome', 'setor_id')
+    form_columns = ('cpf', 'nome', 'setor_id')
+
+class SetorModel(ModelView):
+    column_display_pk = True
+    column_list = ('id', 'nome', 'pai_id')
+    form_columns = ('id', 'nome', 'pai_id')
+    # inline_models = ['', ]
 
 def admin_app(app, session):
     # set optional bootswatch theme
@@ -13,4 +20,4 @@ def admin_app(app, session):
     admin = Admin(app, name='Ficha de Carga', template_mode='bootstrap3')
     # Add administrative views here
     admin.add_view(UserModel(Usuario, session))
-    admin.add_view(ModelView(Setor, session))
+    admin.add_view(SetorModel(Setor, session))
