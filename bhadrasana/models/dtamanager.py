2a1440c745904fc7417bbfc67fc7064252b5c275
IvanBrasilico <brasilico.ivan@gmail.com>
2020-04-15 08:57:51 -0300

    Modificações DTA

--- bhadrasana/models/dtamanager.py
+++ bhadrasana/models/dtamanager.py
@@ -1,3 +1,11 @@
+import io
+from _md5 import md5
+
+import fitz
+from PIL import Image
+from ajna_commons.flask.log import logger
+from gridfs import GridFS
+
 from bhadrasana.models.dta import Anexo
 
 
@@ -33,3 +41,37 @@ def gera_objeto(object, session, params):
         session.rollback()
         raise err
     return object
+
+
+def insert_pagina(mongodb, image: Image,
+                  numero_dta: str, filename: str, pagina: int):
+    fs = GridFS(mongodb)
+    content = io.BytesIO(image)
+    m = md5()
+    m.update(content)
+    grid_out = fs.find_one({'md5': m.hexdigest()})
+    if grid_out:
+        if grid_out.filename == filename:
+            logger.warning(
+                ' Arquivo %s Pagina %s MD5 %s ' +
+                ' tentativa de inserir pela segunda vez!!' %
+                (filename, pagina, m.hexdigest())
+            )
+            # File exists, abort!
+            return grid_out._id
+    # Insert File
+    params = {'numero_dta': numero_dta, 'pagina': pagina}
+    return fs.put(content, filename=filename,
+                  metadata=params)
+
+
+def get_pagina(mongodb, numero_dta: str, filename: str, pagina: int):
+    params = {'filename': filename,
+              'numero_dta': numero_dta,
+              'pagina': pagina}
+    _id = mongodb.fs.files.find(params, {'_id': 1})
+    fs = GridFS(mongodb)
+    return fs.get(_id)
+
+def processa_pdf(filename:str):
+    for pagina in
