18184742ad1c5b2c28e19f6099eff633021fe57f
IvanBrasilico <brasilico.ivan@gmail.com>
2020-04-15 11:43:40 -0300

    Gerar PNGs a partir de um PDF vinculado a uma DTA e gravar no Mongo - protótipo

--- bhadrasana/models/dtamanager.py
+++ bhadrasana/models/dtamanager.py
@@ -3,8 +3,12 @@ from _md5 import md5
 
 import fitz
 from PIL import Image
-from ajna_commons.flask.log import logger
 from gridfs import GridFS
+from pymongo import MongoClient
+
+import sys
+sys.path.insert(0, '../ajna_docs/commons')
+from ajna_commons.flask.log import logger
 
 from bhadrasana.models.dta import Anexo
 
@@ -43,10 +47,10 @@ def gera_objeto(object, session, params):
     return object
 
 
-def insert_pagina(mongodb, image: Image,
-                  numero_dta: str, filename: str, pagina: int):
+def insert_pagina(mongodb, png_image: bytes,
+                  numero_dta: str, filename: str, npagina: int):
     fs = GridFS(mongodb)
-    content = io.BytesIO(image)
+    content = png_image
     m = md5()
     m.update(content)
     grid_out = fs.find_one({'md5': m.hexdigest()})
@@ -55,23 +59,46 @@ def insert_pagina(mongodb, image: Image,
             logger.warning(
                 ' Arquivo %s Pagina %s MD5 %s ' +
                 ' tentativa de inserir pela segunda vez!!' %
-                (filename, pagina, m.hexdigest())
+                (filename, npagina, m.hexdigest())
             )
             # File exists, abort!
             return grid_out._id
     # Insert File
-    params = {'numero_dta': numero_dta, 'pagina': pagina}
+    params = {'numero_dta': numero_dta, 'pagina': npagina}
     return fs.put(content, filename=filename,
                   metadata=params)
 
 
-def get_pagina(mongodb, numero_dta: str, filename: str, pagina: int):
+def get_npaginas(mongodb, numero_dta: str, filename: str, npagina: int):
+    params = {'filename': filename,
+              'numero_dta': numero_dta}
+    return mongodb.fs.files.count_documents(params)
+
+def get_pagina(mongodb, numero_dta: str, filename: str, npagina: int):
     params = {'filename': filename,
               'numero_dta': numero_dta,
-              'pagina': pagina}
+              'pagina': npagina}
     _id = mongodb.fs.files.find(params, {'_id': 1})
+    if _id is None:
+        raise KeyError('Página não encontrada com os parâmetros: ' % params)
     fs = GridFS(mongodb)
-    return fs.get(_id)
+    return Image.open(fs.get(_id))
 
-def processa_pdf(filename:str):
-    for pagina in
+def processa_pdf(mongodb, numero_dta: str, filename: str):
+    pdf = fitz.open(filename)
+    for npagina, page in enumerate(pdf, 1):
+        pix = page.getPixmap()
+        insert_pagina(mongodb, pix.getPNGData(),
+                      numero_dta, filename, npagina)
+    return npagina
+
+
+
+if __name__ == '__main__':
+    mongodb = MongoClient('10.68.64.10')
+    conn = mongodb['transito']
+    npaginas = processa_pdf(mongodb, '1234', 'teste.pdf')
+    npaginas = get_npaginas(mongodb, '1234', 'teste.pdf')
+    for i in range(1, npaginas + 1):
+        image = get_pagina(mongodb, '1234', 'teste.pdf', i)
+        image.save('%s.png' % i)
