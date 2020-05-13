577bf5939d01199165f85e9a0f5c353705410869
IvanBrasilico <brasilico.ivan@gmail.com>
2020-05-13 15:35:03 -0300

    Exibir DUEs na tela consulta contêiner - Pesquisar Dues e imagens pelo campo numeroinformado para o caso de não haver integração do CARGA no Registro.

--- bhadrasana/models/virasana_manager.py
+++ bhadrasana/models/virasana_manager.py
@@ -44,7 +44,7 @@ def get_imagens_container(mongodb, numero: str) -> list:
 def get_dues_container(mongodb, numero: str) -> list:
     if numero is None or numero == '':
         raise ValueError('get_dues: Informe o número do contêiner!')
-    query = {'metadata.carga.container.container': numero.lower().strip(),
+    query = {'metadata.numeroinformado': numero.strip(),
              'metadata.contentType': 'image/jpeg'
              }
     projection = {'metadata.due': 1}
