316f17d3775b3c3bf85cd876d1817a2a5fd36cbd
IvanBrasilico <Iv@n1234>
2020-05-21 16:06:08 -0300

    Exibição dos Eventos da API Recintos na pesquisa_container

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Flask-session',
         'Flask-wtf',
         'gunicorn',
+        'json2html',
         'lxml',
         'Pillow',   # Centralizar acesso imagens no virasana??
         'plotly', # Ver como retirar necessidade
