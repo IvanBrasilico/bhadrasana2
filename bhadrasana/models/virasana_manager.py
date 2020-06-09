6865898f300013d330516a74be50687872668967
IvanBrasilico <brasilico.ivan@gmail.com>
2020-06-09 11:48:08 -0300

    Programação de verificação física através dos dados do AJNA - Chave DUE  TG-93 #in-progress
    Testes passando, falta testar com dados reais do Virasana

--- bhadrasana/models/virasana_manager.py
+++ bhadrasana/models/virasana_manager.py
@@ -41,6 +41,21 @@ def get_imagens_container(mongodb, numero: str) -> list:
     return list(cursor)
 
 
+def get_due(mongodb, numerodeclaracao: str) -> dict:
+    due = {}
+    if numerodeclaracao:
+        cursor = mongodb.fs.files.find(
+            {'metadata.due.numero': numerodeclaracao},
+            {'metadata.due': 1})
+        due = list(cursor)
+        if due and len(due) > 0:
+            due = due[len(due) - 1]
+            due = due.get('metadata')
+            if due:
+                due = due.get('due')[0]
+    return due
+
+
 def get_dues_container(mongodb, numero: str) -> list:
     if numero is None or numero == '':
         raise ValueError('get_dues: Informe o número do contêiner!')
@@ -77,26 +92,47 @@ def get_dues_empresa(mongodb, cnpj: str) -> list:
     return result
 
 
-def get_imagens(mongodb, conhecimento: str) -> dict:
+def get_imagens_query(mongodb, query: dict) -> list:
+    """Retorna cursor da query, campos: _id, numeroinformado, dataescaneamento."""
+    projection = {'metadata.numeroinformado': 1,
+                  'metadata.dataescaneamento': 1}
+    try:
+        cursor = mongodb['fs.files'].find(query, projection)
+    except Exception as err:
+        logger.error(err, exc_info=True)
+        return []
+    return cursor
+
+
+def get_imagens_conhecimento(mongodb, conhecimento: str) -> list:
     if conhecimento is None or conhecimento == '':
         raise ValueError('get_imagens: Informe o conhecimento!')
     query = {'metadata.carga.conhecimento.conhecimento': conhecimento,
              'metadata.contentType': 'image/jpeg'
              }
-    projection = {'metadata.numeroinformado': 1,
-                  'metadata.dataescaneamento': 1}
-
-    try:
-        cursor = mongodb['fs.files'].find(query, projection)
-    except Exception as err:
-        logger.error(err, exc_info=True)
-        return {}
-    return cursor
+    return get_imagens_query(mongodb, query)
 
 
-def get_imagens_dict_container_id(mongodb, conhecimento: str) -> dict:
+def get_imagens_due(mongodb, due: str) -> list:
+    if due is None or due == '':
+        raise ValueError('get_imagens: Informe o conhecimento!')
+    query = {'metadata.due': due,
+             'metadata.contentType': 'image/jpeg'
+             }
+    return get_imagens_query(mongodb, query)
+
+
+def get_imagens_dict_container_id(mongodb, conhecimento: str, due: str) -> dict:
     """Retorna lista de imagens no formato numeroContainer: _id imagem"""
-    imagens = get_imagens(mongodb, conhecimento)
+    try:
+        imagens_conhecimento = get_imagens_conhecimento(mongodb, conhecimento)
+    except ValueError:
+        imagens_conhecimento = []
+    try:
+        imagens_due = get_imagens_due(mongodb, due)
+    except ValueError:
+        imagens_due = []
+    imagens = [*imagens_conhecimento, *imagens_due]
     return {item['metadata']['numeroinformado']: str(item['_id'])
             for item in imagens}
 
