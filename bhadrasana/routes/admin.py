6668ea396aca89eb95f69def8af9e84c21c8608a
IvanBrasilico <brasilico.ivan@gmail.com>
2020-06-24 11:44:11 -0300

    Bug perfil cadastrador

--- bhadrasana/routes/admin.py
+++ bhadrasana/routes/admin.py
@@ -1,10 +1,11 @@
 from flask import url_for, request
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.menu import MenuLink
 from flask_login import current_user
 from werkzeug.utils import redirect
 
-from bhadrasana.models import Enumerado as ModelEnumerado
+from bhadrasana.models import Enumerado as ModelEnumerado, usuario_tem_perfil, perfilAcesso, get_usuario_logado
 from bhadrasana.models import Setor, Usuario, PerfilUsuario
 from bhadrasana.models.ovr import Marca, RoteiroOperacaoOVR, TipoEventoOVR, Enumerado
 
@@ -18,7 +19,31 @@ class ProtectedModelView(ModelView):
         return redirect(url_for('login', next=request.url))
 
 
-class UsuarioModel(ProtectedModelView):
+class CadastradorModelView(ProtectedModelView):
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            result = usuario_tem_perfil(
+                self.session,
+                current_user.id,
+                ModelEnumerado.get_id(perfilAcesso, 'Cadastrador')
+            )
+            # print('*****************' + str(result))
+            return result
+        return False
+
+
+class SupervisorModelView(ProtectedModelView):
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            return usuario_tem_perfil(
+                self.session,
+                current_user.id,
+                ModelEnumerado.get_id(perfilAcesso, 'Supervisor')
+            )
+        return False
+
+
+class UsuarioModel(CadastradorModelView):
     can_delete = False
     column_hide_backrefs = False
     column_searchable_list = ['nome']
@@ -27,7 +52,7 @@ class UsuarioModel(ProtectedModelView):
     form_columns = ('cpf', 'nome', 'telegram', 'setor')
 
 
-class PerfilUsuarioModel(ProtectedModelView):
+class PerfilUsuarioModel(CadastradorModelView):
     column_hide_backrefs = False
     column_searchable_list = ['cpf', 'perfil']
     column_filters = ['cpf', 'perfil']
@@ -35,14 +60,17 @@ class PerfilUsuarioModel(ProtectedModelView):
     form_choices = {'perfil': ModelEnumerado.perfilAcesso()}
 
     def validate_form(self, form):
-        form.perfil.data = int(form.perfil.data)
+        try:
+            form.perfil.data = int(form.perfil.data)
+        except AttributeError:  # Para não dar erro em exclusáo (DeleteForm)
+            pass
         return super(PerfilUsuarioModel, self).validate_form(form)
 
     def on_form_prefill(self, form, id):
         form.perfil.data = str(form.perfil.data)
 
 
-class SetorModel(ProtectedModelView):
+class SetorModel(SupervisorModelView):
     can_delete = False
     column_display_pk = True
     # column_hide_backrefs = False
@@ -57,7 +85,7 @@ class MarcasModel(ProtectedModelView):
     column_searchable_list = ['nome']
 
 
-class RoteirosModel(ProtectedModelView):
+class RoteirosModel(SupervisorModelView):
     column_filters = ['tipooperacao']
     column_hide_backrefs = False
     column_list = ('tipooperacao', 'descricao_tipooperacao', 'descricao',
@@ -65,14 +93,17 @@ class RoteirosModel(ProtectedModelView):
     form_choices = {'tipooperacao': Enumerado.tipoOperacao()}
 
     def validate_form(self, form):
-        form.tipooperacao.data = int(form.tipooperacao.data)
+        try:
+            form.tipooperacao.data = int(form.tipooperacao.data)
+        except AttributeError:  # Para não dar erro em exclusáo (DeleteForm)
+            pass
         return super(RoteirosModel, self).validate_form(form)
 
     def on_form_prefill(self, form, id):
         form.tipooperacao.data = str(form.tipooperacao.data)
 
 
-class TipoEventoModel(ProtectedModelView):
+class TipoEventoModel(SupervisorModelView):
     can_delete = False
     column_list = ('nome', 'descricao', 'descricao_fase', 'eventoespecial')
     form_columns = ('nome', 'descricao', 'fase')
@@ -86,6 +117,11 @@ class TipoEventoModel(ProtectedModelView):
         form.fase.data = str(form.fase.data)
 
 
+class LogoutMenuLink(MenuLink):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
 def admin_app(app, session):
     # set optional bootswatch theme
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -97,3 +133,10 @@ def admin_app(app, session):
     admin.add_view(MarcasModel(Marca, session))
     admin.add_view(RoteirosModel(RoteiroOperacaoOVR, session))
     admin.add_view(TipoEventoModel(TipoEventoOVR, session))
+
+    admin.add_link(LogoutMenuLink(name='Janela principal',
+                                  url='/bhadrasana2', category='Ir para'))
+    if current_user and current_user.is_authenticated:
+        user = get_usuario_logado(session, current_user.id)
+        admin.add_link(LogoutMenuLink(name='Sair (%s)' % user.nome,
+                                      url=url_for('commons.logout'), category='Ir para'))
