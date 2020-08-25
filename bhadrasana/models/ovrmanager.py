5ea7cbb3d3542fde31fc545f98fb91c5b3a43d5d
IvanBrasilico <brasilico.ivan@gmail.com>
2020-08-25 16:20:02 -0300

    Formatar colunas de valor no resultado do relatório

--- bhadrasana/models/ovrmanager.py
+++ bhadrasana/models/ovrmanager.py
@@ -95,7 +95,7 @@ def executa_relatorio(session, user_name: str, relatorio: Relatorio,
     for row in rows:
         formated_cols = []
         for col in row:
-            print(col, type(col))
+            logger.info(str(col) + str(type(col)))
             if isinstance(col, float):
                 fcol = '{:,.2f}'.format(col)
                 fcol = fcol.replace(',', '-').replace('.', ',').replace('-', '.')
