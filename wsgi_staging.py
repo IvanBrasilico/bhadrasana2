be67e90dc2b015d97555248a8e5fc7a455528792
IvanBrasilico <brasilico.ivan@gmail.com>
2020-10-15 09:40:50 -0300

    Servidor - voltar aos bancos antigos

--- wsgi_staging.py
+++ wsgi_staging.py
@@ -6,7 +6,7 @@ sys.path.insert(0, '../ajna_docs/commons')
 sys.path.insert(0, '../virasana')
 
 os.environ['SQL_URI'] = os.environ['SQL_URI_HOM']
-os.environ['MONGODB_URI'] = os.environ['MONGODB_URI_HOM']
+# os.environ['MONGODB_URI'] = os.environ['MONGODB_URI_HOM']
 os.environ['MONGODB_RISCO'] = os.environ['MONGODB_URI_HOM']
 print(os.environ['MONGODB_URI'])
 print(os.environ['MONGODB_RISCO'])
