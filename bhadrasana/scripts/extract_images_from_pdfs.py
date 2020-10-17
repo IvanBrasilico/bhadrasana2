fdea15366c3a8d551ded55ed4d999047321fbc06
IvanBrasilico <brasilico.ivan@gmail.com>
2020-10-17 17:07:09 -0300

    Script para extrair imagens de PDFs - Código para gerar tabela ovr_docx

---
+++ bhadrasana/scripts/extract_images_from_pdfs.py
@@ -0,0 +1,17 @@
+import os
+import fitz
+
+filelist = os.listdir('pdfs')
+for file in filelist:
+    doc = fitz.open(os.path.join('pdfs', file))
+    for i in range(len(doc)):
+        for img in doc.getPageImageList(i):
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            if pix.n < 5:       # this is GRAY or RGB
+                pix.writePNG("imgs/p%s-%s.png" % (i, xref))
+            else:               # CMYK: convert to RGB first
+                pix1 = fitz.Pixmap(fitz.csRGB, pix)
+                pix1.writePNG("imgs/p%s-%s.png" % (i, xref))
+                pix1 = None
+            pix = None
