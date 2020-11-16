50cc83afe433c595fa02f10af88d4a5b90373ea8
22462913807 <adriano.castanheira@gmail.com>
2020-11-16 17:24:31 -0300

    configura variável de ambiente SESSION_COOKIE para homologação

--- wsgi_staging.py
+++ wsgi_staging.py
@@ -8,8 +8,10 @@ sys.path.insert(0, '../virasana')
 os.environ['SQL_URI'] = os.environ['SQL_URI_HOM']
 # os.environ['MONGODB_URI'] = os.environ['MONGODB_URI_HOM']
 os.environ['MONGODB_RISCO'] = os.environ['MONGODB_URI_HOM']
+os.environ['SESSION_COOKIE'] = os.environ['SESSION_COOKIE_HOM']
 print(os.environ['MONGODB_URI'])
 print(os.environ['MONGODB_RISCO'])
+print(os.environ['SESSION_COOKIE'])
 
 from bhadrasana.main import app
 
