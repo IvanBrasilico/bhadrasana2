abbf05831fb6b134b8efc99b566a8b78d224ab1c
IvanBrasilico <brasilico.ivan@gmail.com>
2020-11-30 15:32:20 -0300

    flake

--- bhadrasana/docx/docx_functions.py
+++ bhadrasana/docx/docx_functions.py
@@ -21,14 +21,6 @@ def gera_taseda(rvf: dict, user_name: str):
     return document
 
 
-def gera_taseda(rvf: dict, user_name: str):
-    conteudo = {'unidade': 'ALFSTS', **rvf}
-    basepath = os.path.dirname(__file__)
-    document = docx.Document(os.path.join(basepath, 'taseda.docx'))
-    docx_replacein(document, conteudo, user_name)
-    return document
-
-
 def gera_comunicado_contrafacao(ovr: dict, user_name: str):
     conteudo = {'unidade': 'ALFSTS', **ovr}
     basepath = os.path.dirname(__file__)
