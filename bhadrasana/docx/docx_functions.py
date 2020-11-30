12c6eb02c20c5c1a35b41552982e6c6a0a39e759
IvanBrasilico <brasilico.ivan@gmail.com>
2020-11-29 21:28:38 -0300

    endpoint comunicado_contrafacao

--- bhadrasana/docx/docx_functions.py
+++ bhadrasana/docx/docx_functions.py
@@ -19,3 +19,19 @@ def gera_taseda(rvf: dict, user_name: str):
     document = docx.Document(os.path.join(basepath, 'taseda.docx'))
     docx_replacein(document, conteudo, user_name)
     return document
+
+
+def gera_taseda(rvf: dict, user_name: str):
+    conteudo = {'unidade': 'ALFSTS', **rvf}
+    basepath = os.path.dirname(__file__)
+    document = docx.Document(os.path.join(basepath, 'taseda.docx'))
+    docx_replacein(document, conteudo, user_name)
+    return document
+
+
+def gera_comunicado_contrafacao(ovr: dict, user_name: str):
+    conteudo = {'unidade': 'ALFSTS', **ovr}
+    basepath = os.path.dirname(__file__)
+    document = docx.Document(os.path.join(basepath, 'comunicado_contrafacao.docx'))
+    docx_replacein(document, conteudo, user_name)
+    return document
