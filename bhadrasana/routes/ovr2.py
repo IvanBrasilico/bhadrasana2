2bf5864ac7609b835e0910f757a03ea91fb22ba3
tiberiodamaso <tiberio.damaso@icloud.com>
2021-02-09 18:09:08 -0300

    TG #269 - Melhorias na interface de encerramento da ficha. Lógica de encerramento com resultado a implementar. Inclusão das apreensões na RVF.

--- bhadrasana/routes/ovr2.py
+++ bhadrasana/routes/ovr2.py
@@ -21,7 +21,7 @@ from bhadrasana.models.ovrmanager import monta_ovr_dict, get_docx_choices, get_d
     inclui_docx, get_ovrs_abertas_flags, get_ovr, MarcaManager, get_ids_flags_contrafacao, \
     get_tiposevento_assistente_choice, gera_eventoovr, get_tgovr_one, lista_tgovr, \
     get_tipos_processo, get_tipos_evento_comfase_choice
-from bhadrasana.models.rvfmanager import lista_rvfovr
+from bhadrasana.models.rvfmanager import lista_rvfovr, get_apreensoes
 from bhadrasana.views import get_user_save_path, valid_file
 
 
