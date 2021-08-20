30a5586556e7231ea31531e29583fe75889e9636
IvanBrasilico <brasilico.ivan@gmail.com>
2021-08-20 12:25:06 -0300

    handle_datahora - error proof adicional

--- bhadrasana/models/__init__.py
+++ bhadrasana/models/__init__.py
@@ -176,19 +176,19 @@ def handle_datahora(params):
     data = params.get('adata', '')
     hora = params.get('ahora', '')
     try:
-        if isinstance(data, str):
-            data = datetime.datetime.strptime(data, '%Y-%m-%d')
+        data = datetime.datetime.strptime(data, '%Y-%m-%d')
+        if data is None:
+            data = datetime.date.today()
     except Exception as err:
         data = datetime.date.today()
         logger.error('handle_datahora: %s' % err)
     try:
-        if isinstance(hora, str):
-            hora = datetime.datetime.strptime(hora, '%H:%M').time()
+        hora = datetime.datetime.strptime(hora, '%H:%M').time()
+        if hora is None:
+            hora = datetime.datetime.now().time()
     except Exception as err:
         hora = datetime.datetime.now().time()
         logger.error('handle_datahora: %s' % err)
-    if hora is None:
-        hora = datetime.datetime.now().time()
     return datetime.datetime.combine(data, hora)
 
 
