0ccd23d19fc6415b5807d06437681227f1140aff
IvanBrasilico <brasilico.ivan@gmail.com>
2023-08-15 18:03:07 -0300

    Framework de risco motoristas - importação de dados da planilha COV

--- bhadrasana/models/apirecintos_risco.py
+++ bhadrasana/models/apirecintos_risco.py
@@ -1,20 +1,21 @@
-import json
 import sys
-from typing import Type, Tuple, Union
 
-import numpy as np
 import pandas as pd
-from dateutil import parser
-from sqlalchemy import BigInteger, Column, DateTime, Boolean, String, UniqueConstraint, exists, Numeric
+from sqlalchemy import BigInteger, Column, String, Integer
 
 sys.path.append('.')
 sys.path.insert(0, '../ajna_docs/commons')
 sys.path.insert(0, '../virasana')
 
-from ajna_commons.flask.log import logger
-from bhadrasana.models import Base, BaseRastreavel, BaseDumpable
+from bhadrasana.models import Base
 
 metadata = Base.metadata
+
+RiscoMotorista = {
+    'ATENÇÃO': '1',
+    'CUIDADO': '2',
+    'BANDIDO': '3'
+}
 
 
 class Motorista(Base):
@@ -26,6 +27,7 @@ class Motorista(Base):
     nome_da_mae = Column(String(50), index=True)
     classificacao = Column(String(1), index=True)  # 1 - Atenção; 2 - Cuidado; 3 - Bandido
     carga = Column(String(100))
+    carga_qtde = Column(Integer())
     descricao_transportadora = Column(String(100))
     observacoes = Column(String(200))
 
@@ -42,25 +44,31 @@ if __name__ == '__main__':  # pragma: no-cover
         session = Session()
         # Sair por segurança. Comentar linha abaixo para funcionar
         # sys.exit(0)
-        metadata.drop_all(engine, [metadata.tables['risco_motoristas'],])
-        metadata.create_all(engine, [metadata.tables['risco_motoristas'],])
-        df_cpfs = pd.read_excel('data/motoristas.xlsx')
-        df_ocorrencias = pd.read_excel('data/motoristas.xlsx', sheet_name='2')
-        print(df_ocorrencias.head())
-        print(len(df_ocorrencias))
+        metadata.drop_all(engine, [metadata.tables['risco_motoristas'], ])
+        metadata.create_all(engine, [metadata.tables['risco_motoristas'], ])
+        df_cpfs = pd.read_excel('notebooks/data/motoristas.xlsx')
+        print('CPFs:')
         print(df_cpfs.head())
         print(len(df_cpfs))
         df_cpfs = df_cpfs.dropna()
         print(len(df_cpfs))
+        df_ocorrencias = pd.read_excel('notebooks/data/motoristas.xlsx', sheet_name='2')
+        print(df_ocorrencias.head())
+        print(f'Ocorrências: {len(df_ocorrencias)}')
         df_ocorrencias = df_ocorrencias.dropna()
         df_ocorrencias = df_ocorrencias[df_ocorrencias.classificacao != 'sem registro ']
-        print(len(df_ocorrencias))
+        print(f'Ocorrências: {len(df_ocorrencias)}')
         df_merge = df_cpfs.merge(df_ocorrencias, on='nome', how='left')
+        df_merge = df_merge.merge(df_merge.groupby(['nome'])['carga'].count().reset_index(), on='nome',
+                                  suffixes=['', '_qtde'])
+        print(df_merge.head())
         print(f'len df_merge: {len(df_merge)}')
         df_merge = df_merge.dropna().drop_duplicates()
         print(f'len df_merge: {len(df_merge)}')
-        df_merge['carga'] = df_merge.groupby(['nome'])['carga'].transform(lambda x: ' '.join(x))
+        df_merge['carga'] = df_merge.groupby(['nome'])['carga'].transform(lambda x: ', '.join(x))
         df_merge = df_merge.drop_duplicates()
-        print(f'len df_merge: {len(df_merge)}')
-        df_merge.to_sql('risco_motoristas', engine)
-
+        df_merge['cpf'] = df_merge['cpf'].apply(lambda x: ''.join([c for c in x if c.isnumeric()]))
+        df_merge['classificacao'] = df_merge['classificacao'].apply(lambda x: RiscoMotorista[x.strip()])
+        print(df_merge.head())
+        print(f'len final df_merge: {len(df_merge)}')
+        df_merge.to_sql('risco_motoristas', engine, if_exists='append', index=False)
