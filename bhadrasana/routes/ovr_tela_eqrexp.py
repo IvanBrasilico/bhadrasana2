4968384e25f26dd52e75a0c0ace2de74eed38ad2
33380367877 <dimitri.carneiro@rfb.gov.br>
2025-10-16 15:38:50 -0300

    filtra setor

--- bhadrasana/routes/ovr_tela_eqrexp.py
+++ bhadrasana/routes/ovr_tela_eqrexp.py
@@ -57,7 +57,8 @@ def configure(app):
             LEFT JOIN ovr_usuarios u
                    ON u.cpf = o.responsavel_cpf
             WHERE tipooperacao=2 -- exportação
-            AND fase=0 OR fase=1 OR fase=2
+            AND fase=0 OR fase=1 OR fase=2 -- 
+            AND setor_id = 3            
             ORDER BY o.create_date DESC, o.id DESC
             LIMIT 200
         """)
